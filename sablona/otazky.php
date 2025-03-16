2329374b599940e45136b32cf487fbd45020279e
MarekLorinc <114012200+MarekLorinc@users.noreply.github.com>
2025-03-16 15:33:12 +0100

    Zmena templatu

---
+++ sablona/otazky.php
@@ -0,0 +1,17 @@
+<?php
+$otazky = [
+"Čo je PHP?",
+"Ako funguje cyklus for v PHP?",
+"Aký je rozdiel medzi echo a print v PHP?",
+"Ako môžem spojiť dva reťazce v PHP?",
+"Čo je to asociatívne pole v PHP?"
+];
+
+$odpovede = [
+"PHP je skriptovací jazyk určený na vývoj dynamických webových stránok.",
+"Cyklus for sa používa na iteráciu kódu určitý počet opakovaní. Napríklad: for (\$i = 0; \$i < 10; \$i++) { echo \$i; }",
+"echo je rýchlejší a nepodporuje návratovú hodnotu, zatiaľ čo print vracia hodnotu 1, takže ho možno použiť vo výrazoch.",
+"Dva reťazce môžeš spojiť pomocou operátora . (bodka). Napríklad: \$vysledok = 'Ahoj' . ' svet!';",
+"Asociatívne pole je pole, kde kľúče nie sú číselné indexy, ale textové reťazce, napríklad: \$uzivatel = ['meno' => 'Ján', 'vek' => 25];"
+];
+?>
